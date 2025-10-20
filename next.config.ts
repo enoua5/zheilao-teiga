955a26eb12a2c58d841d2e8c1f06da5649571398
Iota <enoua5@gmail.com>
2025-10-19 19:13:27 -0700

    It needs a subdirectory

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
+    basePath: "/zheilao-teiga",
+    assetPrefix: "/zheilao-teiga",
+    distDir: "/zheilao-teiga",
 };
 
 export default nextConfig;
